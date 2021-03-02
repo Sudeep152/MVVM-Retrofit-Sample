b0659a724143368acae9f7d132a6c134b3a81cc8
Shashank Kumar Dahiya <63150026+Sudeep152@users.noreply.github.com>
2021-03-02 15:35:33 +0530

    ui

--- app/src/main/java/com/sudeep/mvvm_retrofit_sample/UI/Auth/LoginActivity.kt
+++ app/src/main/java/com/sudeep/mvvm_retrofit_sample/UI/Auth/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.sudeep.mvvm_retrofit_sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.sudeep.mvvm_retrofit_sample.UI.Auth.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
--- app/src/main/java/com/sudeep/mvvm_retrofit_sample/UI/Auth/SignupActivity.kt
+++ app/src/main/java/com/sudeep/mvvm_retrofit_sample/UI/Auth/SignupActivity.kt
@@ -1,12 +1,19 @@
 package com.sudeep.mvvm_retrofit_sample.UI.Auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.sudeep.mvvm_retrofit_sample.LoginActivity
 import com.sudeep.mvvm_retrofit_sample.R
 
 class SignupActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+
     }
 }
